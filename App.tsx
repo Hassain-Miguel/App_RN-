dcd714c1f60b5cb534f1e6cf40b1e1e152902c88
Hassain Miguel <141269670+Hassain-Miguel@users.noreply.github.com>
2025-08-13 23:06:56 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React, { JSX } from "react";
+
+// Importar o arquivo com as rotas
+import Routes from "./src/routes";
+
+const App = (): JSX.Element => {
+  return <Routes />
+}
+
+export default App;
